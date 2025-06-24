fd6b540830d1e1490425a031b64fc33554c06193
giakoumv <72873432+giakoumv@users.noreply.github.com>
2025-06-24 12:12:06 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Thalassinos from './Thalassinos';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Thalassinos />
+  </React.StrictMode>
+);
